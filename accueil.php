e7f11628e7937e6b070a57420f9a555e51ef16b7
ktabur <ktabur@THOR11.cfcci53.fr>
2014-03-21 16:18:35 +0100

    Home

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <link href="style.css" rel="stylesheet" />
+        <meta charset="UTF-8">
+        <title> Accueil </title>
+    </head>
+    <body>
+        
+        <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <h1><center>Accueil</center></h1> 
+        <FONT color="red">Connexion/Inscription</FONT>
+        
+    </body>
+    
+    
+    
+</html>
