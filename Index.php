add0c35226fd412aac341148939f1363a50aa3a0
Florian Samson <floriansamson@gmail.com>
2014-03-21 15:08:53 +0100

    structure

--- Index.php
+++ Index.php
@@ -1,10 +1,33 @@
 <?php
+// Charge les librairies
+function __autoload($pClassName)
+{
+    // Modifie My_Db en My/Db
+    // Modifie My_Entity_Promotion en My/Entity/Promotion
+    $pClassName = str_replace('_', '/', $pClassName);
+    // Modifie My/Db en My/Db.php
+    // Modifie My/Entity/Promotion en My/Entity/Promotion.php
+    $pClassName = $pClassName . '.php';
+    // Charge ../application/models/My/Db.php
+    // Charge ../application/models/My/Entity/Promotion.php
+    require '../application/models/' . $pClassName;
+}
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+$controller = '../application/controllers/ctrl-' . $_GET['page'] . '.php';
+$view       = '../application/views/view-' . $_GET['page'] . '.php';
 
-// killian
+// Teste si le controller existe
+if (is_file($controller) == true) {
+    // Charge le controller (les traitements)
+    require $controller;
+    // Teste si la vue existe
+    if (is_file($view) == true) {
+        // Charge la vue (le rendu HTML)
+        require $view;
+    }
+} else {
+    // Générer une page d'erreur
+    echo 'PAGE ERREUR';
+}
+?>
---
+++ application/views/view-inscription.php
@@ -0,0 +1,35 @@
+
+<html>
+    <head>
+        <title>Inscription</title>
+        <meta charset="UTF-8">        
+    </head>
+    <body>
+        <h1>Inscription</h1>
+        <div>
+            <label for="prenom_utilisateur">Prénom :</label>
+            <input id="prenom_utilisateur" type="text" name="prenom" />
+        </div>
+            
+        <div>
+            <label for="nom_utilisateur">Nom :</label>
+            <input id="nom_utilisateur" type="text" name="nom" />
+        </div>
+        
+        <div>
+            <label for="email_utilisateur">Mail :</label>
+            <input id="email_utilisateur" type="text" name="mail" />
+        </div>      
+        
+        <div>
+            <label for="tel_utilisateur">Tel :</label>
+            <input id="tel_utilisateur" type="text" name="tel" />
+        </div>
+        
+        <div>
+            <label for="mdp_utilisateur">Mot de passe :</label>
+            <input id="mdp_utilisateur" type="text" name="mdp" />
+        </div>
+        
+    </body>
+</html>
